158f590f7da3ede55db89625ddb6586ff653048d
fauzan rahman <93891623+zann17@users.noreply.github.com>
2022-03-10 20:53:28 +0700

    Add files via upload

---
+++ Praktikum-1/from10.php
@@ -0,0 +1,16 @@
+<h3>Form Isian Kesehatan</h3>
+<from method="POST" action="proses10.php">
+    Nama : <input type="text" name"nama" value="" size="30"/>
+<br/>
+Berat Badan : <input type="number" value="" size="10" name="berat"/>
+<br/>
+Tinggi Badan : <input type="number" value="" size="10" name="tinggi"/>
+<br/>Usia : <input type="number" value="" size="10" name="usia"/>
+Gol Darah :
+<input type="radio" value="A" name="gol"/>A
+<input type="radio" value="B" name="gol"/>B
+<input type="radio" value="AB" name="gol"/>AB
+<input type="radio" value="O" name="gol"/>O
+<br/>
+<input type="submit" value="kirim" name="proses"/>
+</form>
